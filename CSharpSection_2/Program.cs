4bf012406785688c98e1a1a6e39afb19b0008967
Luis García Figueres <garciafigueres@gmail.com>
2020-04-13 12:44:35 +0200

    interfaces implementation

--- CSharpSection_2/Program.cs
+++ CSharpSection_2/Program.cs
@@ -5,9 +5,13 @@ namespace CSharpSection_2
 {
     class Program
     {
+        static IOperations am;
         static void Main(string[] args)
         {
             Console.WriteLine(SimpleMath.Division(432.23f, 54523.2f));
+
+            am = new AdvancedMath();
+            Console.WriteLine(am.Remainder(7, 3));
 
             BankAccount bankAccount1 = new BankAccount(124321.32f, "Jane Doe");
             Console.WriteLine(bankAccount1.Balance);
@@ -48,6 +52,14 @@ namespace CSharpSection_2
         public static float Division(float n1, float n2)
         {
             return n1 / n2;
+        }
+    }
+
+    class AdvancedMath : SimpleMath, IOperations
+    {
+        public float Remainder(float dividend, float divisor)
+        {
+            return dividend % divisor;
         }
     }
 }
